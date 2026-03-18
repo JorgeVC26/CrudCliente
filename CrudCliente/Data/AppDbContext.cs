3afe1f58263e04f82b4766705d4ca600b951950f
Jorge Villalobos <jorvillacar26@gmail.com>
2026-03-18 12:01:23 -0600

    create db context

---
+++ CrudCliente/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CrudCliente.Models;
+using System.Collections.Generic;
+
+namespace CrudCliente.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
