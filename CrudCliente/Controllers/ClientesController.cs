0c42b1639040128b0dab4c701a14bf9b5668cd9e
Jorge Villalobos <jorvillacar26@gmail.com>
2026-03-18 14:53:14 -0600

    create api controller

---
+++ CrudCliente/Controllers/ClientesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using CrudCliente.Data;
+using CrudCliente.Models;
+
+
+namespace CrudCliente.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ClientesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Clientes.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Cliente customer)
+        {
+            _context.Clientes.Add(customer);
+            _context.SaveChanges();
+            return Ok(customer);
+        }
+    }
+}
